3eb21d3595fe03bd4ac7ee854d1317ee18ae0f76
Serj Baturin <sergejbaturin1985@gmail.com>
2019-09-24 05:37:14 +0500

    Add The First Build

---
+++ todo-app/precache-manifest.04941b6c500d3f444b291521eb14761f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671a82b0167ebb635b72a5a8d5e6dbec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141caa381bb921791289",
+    "url": "/static/css/main.5cd1ca28.chunk.css"
+  },
+  {
+    "revision": "9167de03ac8e38d818d7",
+    "url": "/static/js/2.6e3a50e6.chunk.js"
+  },
+  {
+    "revision": "141caa381bb921791289",
+    "url": "/static/js/main.2bb096e6.chunk.js"
+  },
+  {
+    "revision": "f4470a1d358b84166157",
+    "url": "/static/js/runtime~main.edc4bf8c.js"
+  },
+  {
+    "revision": "c65f38463761106d1806494f3b8bc65e",
+    "url": "/static/media/fon0.c65f3846.jpg"
+  }
+]);
